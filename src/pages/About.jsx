6ff3025f8ea230d1b74d8a7230a77d8c91dae295
KeithCheng22 <keithcheng.sgs@gmail.com>
2024-02-24 15:40:32 +0800

    fix about css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
             <section className='about--section'>
                 <h1 style={{margin:0}}>AEK Communications<button className='soundIconButton' onClick={() => audio.play()}><img className='soundIcon' src={soundIcon} alt='sound icon.'/></button></h1>
                 <span>/eɪ ɛ k ˌkəˌmjuːnɪˈkeɪʃənz/</span>
-                <p>Established in 2007, we excel through a dedicated team and commitment to excellence. We offer comprehensive Telecommunication Solutions, including <b>PABX</b>, <b>Door Access</b>, <b>CCTV</b>, <b>Auto Attendant</b>, and more. Our Slogan: <b><i>"Making Us Closer"</i></b> embodies our steadfast commitment to fostering enduring relationships and ensuring unparalleled client satisfaction. Count on us for reliable products and services to elevate your business.</p>
+                <p className='about-desc'>Established in 2007, we excel through a dedicated team and commitment to excellence. We offer comprehensive Telecommunication Solutions, including <b>PABX</b>, <b>Door Access</b>, <b>CCTV</b>, <b>Auto Attendant</b>, and more. Our Slogan: <b><i>"Making Us Closer"</i></b> embodies our steadfast commitment to fostering enduring relationships and ensuring unparalleled client satisfaction. Count on us for reliable products and services to elevate your business.</p>
                 <div className='about--explore'>
                     <h2 style={{margin: 0, fontSize: '1.2rem'}}>Your connection awaits, our team is ready.</h2>
                     <h2 style={{fontSize: '1.2rem'}}>Our advanced solutions are prepared to take your telecommunications experience to the next level.</h2>
